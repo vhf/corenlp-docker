668ae137727d43702e0bad51cf681eee53d8e037
Victor Felder <victor@draft.li>
2017-01-08 15:06:52 +0100

    Better models handling

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM phusion/baseimage:0.9.19
+FROM phusion/baseimage:latest
 
 # Use baseimage-docker's init system.
 CMD ["/sbin/my_init"]
--- corenlp.sh
+++ corenlp.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+cd /models;
+for jar in *.jar; do
+  ln -s /models/$jar /opt/corenlp/src/$jar;
+done
+
 cd /opt/corenlp/src
-cp /models/* /opt/corenlp
-exec java -mx8g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
+exec java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
